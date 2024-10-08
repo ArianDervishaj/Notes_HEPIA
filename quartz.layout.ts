7eb70164bc001bc2afe7f56f35d77791540380d0
Arian <ArianDervishaj@icloud.com>
2024-10-08 21:44:44 +0200

    Quartz sync: Oct 8, 2024, 9:44 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -46,6 +46,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.TableOfContents())
   ],
   right: [],
 }
