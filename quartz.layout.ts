379bd7410716d0c87b9f673452f317803c2dd53d
Arian <ArianDervishaj@icloud.com>
2024-10-08 21:04:10 +0200

    Quartz sync: Oct 8, 2024, 9:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,8 +17,9 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
     Component.MobileOnly(Component.Explorer()),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -38,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(),Component.MobileOnly(Component.Explorer()), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.MobileOnly(Component.Explorer()),Component.MobileOnly(Component.Spacer()), Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
